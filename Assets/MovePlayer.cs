cadcb3a065c316424227d85c42c7725e14281ae7
neDanila228 <nesterovd966@gmail.com>
2026-02-17 10:53:12 +0500

    Первый запуск проекта

---
+++ Assets/MovePlayer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(moveX, 0, moveZ);
+        rb.velocity = move * speed;
+    }
+}
---
+++ Assets/PhotoCapture.cs
@@ -0,0 +1,64 @@
+using TMPro;
+using UnityEngine;
+using System.Collections;
+public class PhotoCapture : MonoBehaviour
+{
+    public int filmCount = 10;
+    public GameObject flashEffect;
+    public TextMeshProUGUI counterText;
+    public GameObject flashUI;
+    public KeyCode switchKey = KeyCode.Tab;
+    public GameObject presentWorld;
+    public GameObject pastWorld;
+
+    private bool isPresent = true;
+    void Update()
+        if (Input.GetKeyDown(switchKey))
+{
+    SwitchTime();
+}
+{
+        if (Input.GetMouseButtonDown(0))
+        {
+            TakePhoto();
+            if (flashUI != null)
+            {
+                StartCoroutine(FlashUICO());
+            }
+        }
+    }
+    
+    void TakePhoto()
+    {
+        if (filmCount <= 0)
+        {
+            Debug.Log("Нет плёнки!");
+            return;
+        }
+
+        filmCount--;
+        if (counterText != null)
+        {
+            counterText.text = filmCount.ToString();
+        }
+        Debug.Log("Щёлк! Осталось: " + filmCount);
+
+        if (flashEffect != null)
+        {
+            Instantiate(flashEffect, transform.position, transform.rotation);
+        }
+    }
+    IEnumerator FlashUICO()
+    {
+        flashUI.SetActive(true);
+        yield return new WaitForSeconds(0.1f);
+        flashUI.SetActive(false);
+    }
+void SwitchTime()
+{
+    isPresent = !isPresent;
+    presentWorld.SetActive(isPresent);
+    pastWorld.SetActive(!isPresent);
+    Debug.Log(isPresent ? "Настоящее" : "Прошлое");
+}
+}
